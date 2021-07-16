bff97e3f73961204ba99f3db0e990bf1fb5d5ff9
Raul <raulmayas20@gmail.com>
2021-07-16 09:00:20 -0500

    Adding to LF1 Json Link and represent it on the map

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -11,3 +11,9 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   id: "mapbox/streets-v11",
   accessToken: API_KEY
 }).addTo(myMap);
+
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+d3.json(link).then(function(data){
+    L.geoJson(data).addTo(myMap);
+})
